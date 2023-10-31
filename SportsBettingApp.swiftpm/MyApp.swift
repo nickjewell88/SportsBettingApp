243dc3a270a0631d9b756a63321e65f2c9d94148
Nick Jewell <jewellnick3@gmail.com>
2023-10-31 10:35:32 -0500

    Oct	31

--- SportsBettingApp.swiftpm/MyApp.swift
+++ SportsBettingApp.swiftpm/MyApp.swift
@@ -1,7 +1,66 @@
 import SwiftUI
 
+struct ContentView: View {
+    var body: some View {
+        NavigationView {
+            List {
+                NavigationLink("Baseball Tools", destination: BaseballToolsView())
+                NavigationLink("Basketball Tool", destination: BasketballToolView())
+                NavigationLink("Costanza", destination: CostanzaView())
+                NavigationLink("Bet Tracker", destination: BetTrackerView())
+                NavigationLink("User Profile", destination: UserProfileView())
+            }
+            .navigationBarTitle("SportsBet Assistant")
+        }
+    }
+}
+
+struct BaseballToolsView: View {
+    var body: some View {
+        VStack {
+            // TODO: Add Expected Runs Calculator UI
+            // TODO: Add True Average Calculator UI
+        }
+        .navigationBarTitle("Baseball Tools")
+    }
+}
+
+struct BasketballToolView: View {
+    var body: some View {
+        // TODO: Add Basketball Tool UI
+        Text("Basketball Tool")
+            .navigationBarTitle("Basketball Tool")
+    }
+}
+
+struct CostanzaView: View {
+    var body: some View {
+        // TODO: Add Costanza UI
+        Text("Costanza")
+            .navigationBarTitle("Costanza")
+    }
+}
+
+struct BetTrackerView: View {
+    var body: some View {
+        VStack {
+            // TODO: Add Bet Input UI
+            // TODO: Add History Viewer UI
+        }
+        .navigationBarTitle("Bet Tracker")
+    }
+}
+
+struct UserProfileView: View {
+    var body: some View {
+        // TODO: Add User Profile UI
+        Text("User Profile")
+            .navigationBarTitle("User Profile")
+    }
+}
+
 @main
-struct MyApp: App {
+struct SportsBetAssistantApp: App {
     var body: some Scene {
         WindowGroup {
             ContentView()
